376c1f2a9b3d33fd882e6e6a9ca56181572ea62f
Aayush-1205 <shindeaayush250@gmail.com>
2024-01-22 13:12:37 -0500

    Done

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:MjQ3NDYtc2hpbmRl@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:MTk1MTgtc2hpbmRl@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from './Components/Navbar/Navbar';
+import Navbar from './Components/Navbar/Navbar.js';
 function App() {
-
+    
   return (
     <div className="App">
         <BrowserRouter>
